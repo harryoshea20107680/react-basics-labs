b9806d7124261eb3d673cccea0e5ae1d0bb7079f
Harry OShea <20107680@setu.ie>
2025-10-17 23:41:52 +0100

    Tasky Lab 2a - Exercise done

--- taskyHOSHEA/src/components/Task.jsx
+++ taskyHOSHEA/src/components/Task.jsx
@@ -4,10 +4,11 @@ const Task = (props) => {
     
     
     return (
-    <div className="card">
+    <div className={`card ${props.priority}`}>
       <p className="title">{props.title}</p>
       <p>Due: {props.deadline}</p>
       <p className="description">{props.description}</p>
+      <p className="priority">Priority: {props.priority}</p>
     </div>
   );
 }
